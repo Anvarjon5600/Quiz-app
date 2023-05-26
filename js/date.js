2c3d10bffc2f5e0225b1ac6beeca8167b486232e
Anvarjon5600 <anvarjonganiyev96@gmail.com>
2023-05-26 17:04:12 +0500

    'new'

---
+++ js/date.js
@@ -0,0 +1,72 @@
+export const questions = [
+    {
+        question: "12*12=?",
+        variants: ["123", "144", "28", "238", "244"],
+        rightAnswer: "144",
+        id: 1,
+        name: "question1"
+    },
+    {
+        question: "10*123?",
+        variants: ["1230", "1260", "1242", "1233", "1230"],
+        rightAnswer: "1230",
+        id: 2,
+        name: "question2"
+    },
+    {
+        question: "234/2?",
+        variants: ["116", "117", "127", "118", "107"],
+        rightAnswer: "117",
+        id: 3,
+        name: "question3"
+    },
+    {
+        question: "1-290=??",
+        variants: ["289", "-289", "-290", "-291", "AYIRIB BO'LMAYDI"],
+        rightAnswer: "-289",
+        id: 4,
+        name: "question4"
+    },
+    {
+        question: "23*3=??",
+        variants: ["64", "66", "69", "68", "62"],
+        rightAnswer: "69",
+        id: 5,
+        name: "question5"
+    },
+    {
+        question: "1000-109=??",
+        variants: ["989", "991", "992", "994", "981"],
+        rightAnswer: "991",
+        id: 6,
+        name: "question6"
+    },
+    {
+        question: "9*8=??",
+        variants: ["72", "1122", "54", "64", "81"],
+        rightAnswer: "72",
+        id: 7,
+        name: "question7"
+    },
+    {
+        question: "3*6=??",
+        variants: ["18", "15", "24", "13", "19"],
+        rightAnswer: "18",
+        id: 8,
+        name: "question8"
+    },
+    {
+        question: "4*504=??",
+        variants: ["2016", "1233", "2123", "2160", "1200"],
+        rightAnswer: "2016",
+        id: 9,
+        name: "question9"
+    },
+    {
+        question: "12/24=??",
+        variants: ["0.5", "2", "0.4", "1.2", "1.4"],
+        rightAnswer: "0.5",
+        id: 10,
+        name: "question10"
+    },
+]
---
+++ js/main.js
@@ -0,0 +1,153 @@
+'use strict'
+
+import { questions } from "./date.js";
+let quizContainer = document.querySelector(".questions-container")
+let form;
+let inputs;
+let offset = 0;
+let right;
+let results = []
+let minutContent = document.querySelector('.minut')
+let secundContent = document.querySelector('.second')
+let min = 5;
+let secund = 0;
+
+
+document.querySelector('.start-btn').addEventListener('click', (e) => {
+  e.target.classList.add('none')
+  document.querySelector('.test-box').classList.remove('none')
+  let start = () => {
+    if (min === 0 && secund === 0) {
+      clearInterval(intervalId)
+      document.querySelector('.test-box').classList.add('none')
+      const trues = results.filter(item => item.ans === true).length;
+      const falsy = results.filter(item => item.ans === false).length
+      if (results.length === 0) resultText.textContent = `Siz savollarga javob bermadiz Hamma savolga javob bering`
+      else if (trues === results.length) { resultText.textContent = `Sizda ${trues} togri javob` }
+      else if (falsy === results.length) {
+        resultText.textContent = `Sizda ${falsy} notogri javob bor
+      ` }
+      else { resultText.textContent = `Sizda ${trues} togri javob va ${falsy} notogri javob bor` }
+      modal.classList.add("modal-top")
+      return;
+    } else {
+      if (secund === 0) {
+        --min
+        secund = 60
+      }
+      --secund
+    }
+
+    if (min - 1 < 0) {
+      minutContent.textContent = `00:`
+    } else if (min < 10) {
+      minutContent.textContent = `0${min}:`;
+    } else minutContent.textContent = min;
+
+    if (min < 0) {
+      secundContent.textContent = '00';
+    } else if (secund < 10) {
+      secundContent.textContent = `0${secund}`;
+    } else secundContent.textContent = secund;
+  }
+  let intervalId = setInterval(start, 1000);
+})
+
+questions.map((item, index) => {
+  form = `
+    <form  class="quiz-form">
+    <h1> ${index + 1}-Savol. ${item.question}</h1>
+        <label value="${item.variants[0]}" for="${item.name}">
+            <input value="${item.variants[0]}" type="radio" name="${item.name}" />
+            <span>${item.variants[0]}</span>
+        </label>
+         <label value="${item.variants[1]}" for="${item.name}">
+           <input value="${item.variants[1]}" type="radio" name="${item.name}" />
+           <span>${item.variants[1]}</span>
+         </label>
+         <label value="${item.variants[2]}" for="${item.name}">
+           <input value="${item.variants[2]}" type="radio" name="${item.name}" />
+           <span>${item.variants[2]}</span>
+         </label>
+         <label value="${item.variants[3]}" for="${item.name}">
+           <input value="${item.variants[3]}" type="radio" name="${item.name}" />
+           <span>${item.variants[3]}</span>
+         </label>
+         <label value="${item.variants[4]}" for="${item.name}">
+           <input value="${item.variants[4]}" type="radio" name="${item.name}" />
+           <span>${item.variants[4]}</span>
+         </label>
+
+    </form >
+    `
+  quizContainer.insertAdjacentHTML("beforeend", form);
+  inputs = document.getElementsByName(`${item.name}`);
+  for (let inp of inputs) {
+    inp.addEventListener("change", function (e) {
+      right = e.target.value === item.rightAnswer
+      let obj = { ans: right, id: index + 1 }
+
+      let some = results.some(item => item.id === obj.id)
+      if (some) {
+        let index = results.findIndex(item => item.id === obj.id)
+        results[index] = { ans: right, id: index + 1 }
+      } else results.push(obj);
+      if (results.length === 10) {
+        console.log('tugadi')
+        nextBtn.textContent = 'End'
+      }
+    })
+  }
+})
+
+let modal = document.querySelector(".modal")
+let resultText = document.querySelector(".result")
+let endBtn = document.getElementById("end");
+
+endBtn.addEventListener("click", function () {
+  document.querySelector('.test-box').classList.add('none')
+  const trues = results.filter(item => item.ans === true).length;
+  const falsy = results.filter(item => item.ans === false).length
+  if (results.length === 0) resultText.textContent = `Siz savollarga javob bermadiz Hamma savolga javob bering`
+  else if (trues === results.length) { resultText.textContent = `Sizda ${trues} togri javob` }
+  else if (falsy === results.length) {
+    resultText.textContent = `Sizda ${falsy} notogri javob bor
+  ` }
+  else { resultText.textContent = `Sizda ${trues} togri javob va ${falsy} notogri javob bor` }
+  modal.classList.add("modal-top")
+})
+
+
+let xBtn = document.querySelector(".x")
+xBtn.addEventListener("click", () => {
+  modal.classList.remove("modal-top")
+})
+
+let nextBtn = document.querySelector(".next")
+let prevBtn = document.querySelector(".prev")
+
+nextBtn.addEventListener("click", function () {
+  if (nextBtn.textContent = 'Next') {
+    offset += 480
+    if (offset > 4320) { offset = 4320 }
+    quizContainer.style.left = -offset + "px"
+  }
+  if (results.length == 10) {
+    document.querySelector('.test-box').classList.add('none')
+    const trues = results.filter(item => item.ans === true).length;
+    const falsy = results.filter(item => item.ans === false).length
+    if (results.length === 0) resultText.textContent = `Siz savollarga javob bermadiz Hamma savolga javob bering`
+    else if (trues === results.length) { resultText.textContent = `Sizda ${trues} togri javob` }
+    else if (falsy === results.length) {
+      resultText.textContent = `Sizda ${falsy} notogri javob bor
+    ` }
+    else { resultText.textContent = `Sizda ${trues} togri javob va ${falsy} notogri javob bor` }
+    modal.classList.add("modal-top")
+  }
+})
+
+prevBtn.addEventListener("click", function () {
+  offset -= 480
+  if (offset < 0) { offset = 0 }
+  quizContainer.style.left = -offset + "px"
+})
